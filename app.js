f1607e4678b230c6ce1d86030505a5142c79279c
djhoo <djhoo@sina.com>
2020-02-15 19:45:19 +0800

    add customer folder

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ var multer  = require('multer');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var service = require('./routes/service');
+var customer = require('./routes/customer');
 
 var app = express();
 
@@ -81,6 +82,7 @@ app.use(function(req,res,next){
 app.use('/', index);
 app.use('/users', users);
 app.use('/service', service);
+app.use('/customer', customer);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
